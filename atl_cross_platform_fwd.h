1af232191b2b490d793a730466bc069ded084ae3
Max Robinson <mcrpublic@gmail.com>
2016-05-11 19:45:24 -0700

    Major update for Windows - Missing files

---
+++ atl_cross_platform_fwd.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "atl_cross_platform_namespace.h"
+
+namespace atl_file_scope = atl_cross_platform_namespace;
+namespace atl_file_scope
+{
+    struct stage;
+    struct touch;
+}
---
+++ atl_cross_platform_namespace.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define atl_namespace atl;
